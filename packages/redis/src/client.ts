54460d4a8977f5f7f6f72959b67f683ec183d2f0
Kaushik Sarkar <akaushikarkar@gmail.com>
2026-02-13 20:58:48 +0530

    removed init redis every time its imported

--- packages/redis/src/client.ts
+++ packages/redis/src/client.ts
@@ -1,10 +1,12 @@
 import Redis from "ioredis";
 
-export const RedisClient = new Redis({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null, // For BullMQ
-  retryStrategy(times) {
-    return Math.min(times * 50, 2000);
-  },
-});
+export function createRedisClient() {
+  return new Redis({
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT) || 6379,
+    maxRetriesPerRequest: null,
+    retryStrategy(times) {
+      return Math.min(times * 50, 2000);
+    },
+  });
+}
