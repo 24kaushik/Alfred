787c006b1fa2b6d167bb8d5fe26467cd9d9706d3
Kaushik Sarkar <akaushikarkar@gmail.com>
2026-02-13 22:19:28 +0530

    setup prisma package correctly to be used across apps

--- apps/erp-adapter/src/app.ts
+++ apps/erp-adapter/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import type { Express } from "express";
 // import loginRouter from "./router/login.route";
+import {prisma} from "@alfred/db";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
 
