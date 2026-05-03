481ed217dd0e9f8ae1fd6f76bb9f311d26eb8445
Kaushik Sarkar <akaushikarkar@gmail.com>
2026-05-03 17:40:54 +0530

    Add rate limiting middleware

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "./types/express.interface"; // Import custom type definitions for Express
+import expressRateLimit from "express-rate-limit";
 
 // Basic setup
 const app: express.Application = express();
@@ -22,11 +23,16 @@ import authRouter from "./router/auth.route";
 import userRouter from "./router/user.route";
 import chatRouter from "./router/chat.route";
 
+app.use(
+  expressRateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 30, // Limit each IP to 30 requests per windowMs
+  }),
+);
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/chat", chatRouter);
-
-
 
 // Error handling
 import errorHandler from "./middleware/errorHandler.middleware";
